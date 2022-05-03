ff7d227f5fbf84985071dabfb8b034c09b60aa1f
PapeAlioune <badaradiakhate14@gmail.com>
2022-05-03 13:23:31 +0000

    VPC module provisionning

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Terraform configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
